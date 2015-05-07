daf3fa6363c90d32f6791f3ee8e8abbd92fe8ad0
Isabel Baltazar <cheska.baltazar@gmail.com>
2015-05-07 16:46:24 +1200

    Filled in some more attributes for Node

--- Node.java
+++ Node.java
@@ -1,3 +1,19 @@
 public class Node {
-  
+  /* Piece name */
+  Piece pieceType;
+  /* Piece location */
+  int[] location;
+  int x;
+  int y;
+  Node up;
+  Node down;
+  Node left;
+  Node right;
+
+  public Node (Piece pieceType, int[] location) {
+    this.pieceType = pieceType;
+    this.location = location;
+    this.x = location[0];
+    this.y = location[1];
+  }
 }
